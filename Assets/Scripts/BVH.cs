9981e04b98a755d87f3f749a1e3118cb7eda26e8
Fengleaf <as61002@gmail.com>
2021-10-26 14:22:06 +0800

    會跳舞了

--- Assets/Scripts/BVH.cs
+++ Assets/Scripts/BVH.cs
@@ -1,11 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MotionFrameList
-{
-
-}
+using UnityEngine.UI;
 
 public class BVH : MonoBehaviour
 {
@@ -14,11 +11,24 @@ public class BVH : MonoBehaviour
 
     public BVHJoint root;
 
+    private List<BVHJoint> joints;
+
+    private int frameNumber;
+    private float frameTime;
+
+    private void Awake()
+    {
+        joints = new List<BVHJoint>();
+        frameNumber = 0;
+        frameTime = 0;
+    }
+
     public void AddRoot(string name)
     {
         root = Instantiate(jointPrefab, Vector3.zero, Quaternion.identity);
         root.gameObject.name = name;
         root.transform.SetParent(transform);
+        joints.Add(root);
     }
 
     public void AddJoint(string name, string parent)
@@ -27,6 +37,8 @@ public class BVH : MonoBehaviour
         BVHJoint joint = Instantiate(jointPrefab, parentJoint.transform);
         joint.gameObject.name = name;
         joint.parentJoint = parentJoint.GetComponent<BVHJoint>();
+        if (!name.Contains("End Site"))
+            joints.Add(joint);
         // 產生joint後，生成骨架
         GameObject bone = Instantiate(bonePrefab, parentJoint.transform);
         bone.name = parentJoint.name + " " + joint.name + " Bone";
@@ -46,5 +58,68 @@ public class BVH : MonoBehaviour
     {
         BVHJoint joint = root.FindJoint(name).GetComponent<BVHJoint>();
         joint.channels = channels;
+    }
+
+    public void SetFrameTime(float time)
+    {
+        frameTime = time;
+    }
+
+    public void AddMotionFrame(List<string> value)
+    {
+        List<float> data = new List<float>();
+        for (int i = 0; i < value.Count; i++)
+            data.Add(Convert.ToSingle(value[i]));
+        int index = 0;
+        for (int i = 0; i < joints.Count; i++)
+        {
+            for (int j = 0; j < joints[i].channels.Count; j++)
+            {
+                joints[i].AddFrames(frameNumber, j, data[index]);
+                index++;
+            }
+        }
+        frameNumber++;
+    }
+
+    public void SetPath()
+    {
+
+    }
+
+    public void Run()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    private IEnumerator RunCoroutine()
+    {
+        while (true)
+        {
+            // 每一個 frame
+            for (int i = 0; i < frameNumber; i++)
+            {
+                GameManager.Instance.frameText.text = (i + 1).ToString();
+                // 每一個 joint
+                for (int j = 0; j < joints.Count; j++)
+                {
+                    try
+                    {
+                        joints[j].UpdateToFrame(i);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError("frame = " + i + ", joint = " + joints[j].name);
+                    }
+
+                }
+                for (int j = 0; j < joints.Count; j++)
+                {
+                    joints[j].UpdateAllBone();
+                }
+                yield return new WaitForSeconds(frameTime);
+            }
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/BVHJoint.cs
+++ Assets/Scripts/BVHJoint.cs
@@ -8,6 +8,8 @@ public class BVHJoint : MonoBehaviour
 
     public List<string> channels = new List<string>();
 
+    public List<Dictionary<int, float>> frames = new List<Dictionary<int, float>>();
+
     public Dictionary<string, GameObject> bones = new Dictionary<string, GameObject>();
 
     public GameObject FindJoint(string name)
@@ -16,7 +18,7 @@ public class BVHJoint : MonoBehaviour
             return gameObject;
         else
         {
-            for (int i = 0;i < transform.childCount;i++)
+            for (int i = 0; i < transform.childCount; i++)
             {
                 BVHJoint child = transform.GetChild(i).GetComponent<BVHJoint>();
                 GameObject target = child?.FindJoint(name);
@@ -36,5 +38,49 @@ public class BVHJoint : MonoBehaviour
         // 縮放
         bone.transform.localScale = new Vector3(1, 1, bone.transform.localPosition.magnitude * 2);
         bone.transform.LookAt(joint.transform);
+    }
+
+    public void UpdateAllBone()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            BVHJoint child = transform.GetChild(i).GetComponent<BVHJoint>();
+            if (child != null)
+                UpdateBone(child.name);
+        }
+    }
+
+    public void AddFrames(int frameNumber, int channelIndex, float value)
+    {
+        if (frameNumber >= frames.Count)
+            frames.Add(new Dictionary<int, float>());
+        frames[frameNumber][channelIndex] = value;
+    }
+
+    public void UpdateToFrame(int frameNumber)
+    {
+        if (frameNumber >= frames.Count)
+            return;
+        Dictionary<int, float> frameData = frames[frameNumber];
+        Vector3 position = transform.localPosition;
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        foreach (KeyValuePair<int, float> pair in frameData)
+        {
+            string channel = channels[pair.Key];
+            if (channel == "Xposition")
+                position.x = pair.Value;
+            else if (channel == "Yposition")
+                position.y = pair.Value;
+            else if (channel == "Zposition")
+                position.z = pair.Value;
+            else if (channel == "Zrotation")
+                rotation.z = pair.Value;
+            else if (channel == "Xrotation")
+                rotation.x = pair.Value;
+            else if (channel == "Yrotation")
+                rotation.y = pair.Value;
+        }
+        transform.localPosition = position;
+        transform.localRotation = Quaternion.Euler(rotation);
     }
 }
