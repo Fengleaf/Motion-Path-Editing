666d86585cac7eb9cdc44fb71be27504d67f3040
ImRTon <col360785@gmail.com>
2022-01-23 17:58:12 +0800

    bug fix

--- Assets/Scripts/BVH/BVHUnityChan.cs
+++ Assets/Scripts/BVH/BVHUnityChan.cs
@@ -29,7 +29,10 @@ public class BVHUnityChan : MonoBehaviour
         {
             Transform TempBonesTran = SearchHumanBoneTransformByName(joints[i].name);
             if (TempBonesTran == null)
+            {
+                Debug.Log("No match: " + joints[i].name);
                 continue;
+            }
 
             // 把 Motion 套上去
             Quaternion org = new Quaternion(MotionPos[CurrentIndex * 4],
@@ -103,7 +106,10 @@ public class BVHUnityChan : MonoBehaviour
     {
         Transform BodyBonesTran = SearchHumanBoneTransformByName(name);
         if (BodyBonesTran == null)
+        {
+            Debug.Log("Bone not found: " + name);
             return;
+        }
 
         // 把關節加進去
         Joints.Add(BodyBonesTran.gameObject);
@@ -124,23 +130,29 @@ public class BVHUnityChan : MonoBehaviour
                 break;
             #region 左下半邊 ( 跟 Motion 的資料顛倒 )
             case "LeftUpLeg":
+            case "LeftHip":
                 temp = HumanBodyBones.RightUpperLeg;
                 break;
             case "LeftLowLeg":
+            case "LeftKnee":
                 temp = HumanBodyBones.RightLowerLeg;
                 break;
             case "LeftFoot":
+            case "LeftAnkle":
                 temp = HumanBodyBones.RightFoot;
                 break;
             #endregion
             #region 右下半邊
             case "RightUpLeg":
+            case "RightHip":
                 temp = HumanBodyBones.LeftUpperLeg;
                 break;
             case "RightLowLeg":
+            case "RightKnee":
                 temp = HumanBodyBones.LeftLowerLeg;
                 break;
             case "RightFoot":
+            case "RightAnkle":
                 temp = HumanBodyBones.LeftFoot;
                 break;
             #endregion
@@ -149,28 +161,34 @@ public class BVHUnityChan : MonoBehaviour
                 temp = HumanBodyBones.Chest;
                 break;
             case "LeftCollar":
+            case "LeftShoulder":
                 temp = HumanBodyBones.RightShoulder;
                 break;
             case "LeftUpArm":
                 temp = HumanBodyBones.RightUpperArm;
                 break;
             case "LeftLowArm":
+            case "LeftElbow":
                 temp = HumanBodyBones.RightLowerArm;
                 break;
             case "LeftHand":
+            case "LeftWrist":
                 temp = HumanBodyBones.RightHand;
                 break;
 
             case "RightCollar":
+            case "RightShoulder":
                 temp = HumanBodyBones.LeftShoulder;
                 break;
             case "RightUpArm":
                 temp = HumanBodyBones.LeftUpperArm;
                 break;
             case "RightLowArm":
+            case "RightElbow":
                 temp = HumanBodyBones.LeftLowerArm;
                 break;
             case "RightHand":
+            case "RightWrist":
                 temp = HumanBodyBones.LeftHand;
                 break;
             #endregion
