55d1082efd9c6d656a5b58e8dcdb6f6983b3c7c1
Fengleaf <as61002@gmail.com>
2022-01-23 15:17:13 +0800

    轉hitps

--- Assets/Scripts/BVH/BVH.cs
+++ Assets/Scripts/BVH/BVH.cs
@@ -170,32 +170,21 @@ public class BVH : MonoBehaviour
             return;
         Dictionary<string, float> frame = root.GetFrameData(pathIndex);
         Dictionary<string, float> nextFrame = root.GetFrameData(pathIndex + 1);
+
+        Vector3 ori = originPathPoint[pathIndex];
+
         Vector3 now = new Vector3(frame[BVHJoint.XPosition], frame[BVHJoint.YPosition], frame[BVHJoint.ZPosition]);
         Vector3 next = new Vector3(nextFrame[BVHJoint.XPosition], nextFrame[BVHJoint.YPosition], nextFrame[BVHJoint.ZPosition]);
-        Vector3 vector = next - now;
-        Vector3 cross = Vector3.Cross(vector, -root.transform.right);
-        Debug.DrawLine(now, next, Color.red, 5f) ;
-        //transform.LookAt(next, Vector3.up);
-        transform.RotateAround(root.transform.position, Vector3.up, 1);
+
+        float costheta = Vector3.Dot(ori, now) / now.magnitude / ori.magnitude;
+        float theta = Mathf.Acos(costheta) * Mathf.Rad2Deg;
+        if (costheta >= 0.999f)
+            theta = 0;
+        Debug.Log(theta);
+        root.transform.RotateAround(root.transform.position, Vector3.up, theta);
+
+        Debug.DrawLine(ori, now, Color.red, 5f) ;
         Debug.DrawLine(root.transform.position, root.transform.position + Vector3.up * 100, Color.green);
-        // 面向切線方向
-        //if (pathIndex < orientationPoints.Count - 1)
-        //{
-        //Vector3 now = orientationPoints[pathIndex];// root.GetPosition(pathIndex);
-        ////Vector3 next = orientationPoints[pathIndex + 1]; //root.GetPosition(pathIndex + 1);
-        //// A 點到 B 點的向量
-        ////Vector3 vector = next - now;
-        //// 外積
-        //Quaternion rotation = Quaternion.Euler(0, 45, 0);
-        //Vector3 rotateVector = rotation * now;
-        //Vector3 cross = Vector3.Cross(now, rotateVector);
-        ////transform.LookAt(transform.position + now, cross);
-        ////transform.rotation = Quaternion.identity;
-        //float angle = Vector3.Angle(originPathPoint[pathIndex], pathPoints[pathIndex]);
-        //transform.rotation = Quaternion.identity;
-        //transform.Rotate(root.transform.position, angle);
-        ////transform.rotation.SetFromToRotation(transform.rotation.eulerAngles, now);
-        //}
     }
 
     public List<Vector3> GetAllPath()
